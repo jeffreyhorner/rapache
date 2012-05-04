2a0c271e70220b1d350dd3cfe1d1d900f3b2f80d
jeffreyhorner <jeffrey.horner@gmail.com>
2012-05-04 10:25:35 -0500

    Release 1.1.20

--- mod_R.c
+++ mod_R.c
@@ -19,7 +19,7 @@
  * Headers and macros
  *
  *************************************************************************/
-#define MOD_R_VERSION "1.1.19"
+#define MOD_R_VERSION "1.1.20"
 #include "mod_R.h" 
 
 #include <sys/types.h>
