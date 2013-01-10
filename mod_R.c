cb1f59075a0a7f8f9cba9e7d890c1f8ccdb79916
jeffreyhorner <jeffrey.horner@gmail.com>
2013-01-10 12:33:25 -0600

    Updating release to 1.2.2

--- mod_R.c
+++ mod_R.c
@@ -19,7 +19,7 @@
  * Headers and macros
  *
  *************************************************************************/
-#define MOD_R_VERSION "1.2.1"
+#define MOD_R_VERSION "1.2.2"
 #include "mod_R.h" 
 
 #include <sys/types.h>
